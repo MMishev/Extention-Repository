1b3fc5bd414f64a445ec8af013c54918eda5fb10
Mishev <m.n.mishev@gmail.com>
2018-11-11 14:23:28 +0200

    initial commit

---
+++ src/main/java/com/repository/ExtentionRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtentionRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtentionRepositoryApplication.class, args);
+    }
+}
